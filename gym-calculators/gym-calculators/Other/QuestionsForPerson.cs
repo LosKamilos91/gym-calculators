3bcf7fe277bdf82265eba82cadc8295834fb71b8
Kamil Orzechowski <k91.orzechowski@gmail.com>
2023-04-16 18:49:17 +0200

    Modified functions in QuestionForPerson.cs

--- gym-calculators/gym-calculators/Other/QuestionsForPerson.cs
+++ gym-calculators/gym-calculators/Other/QuestionsForPerson.cs
@@ -2,16 +2,61 @@
 {
     internal static class QuestionsForPerson
     {
+        
         private static T ConvertTypeForQuestion<T>(this object value)
         {
-            return (T)Convert.ChangeType(value, typeof(T));
+            bool error = true;
+            while (error)
+            {
+                try
+                {
+                    return (T)Convert.ChangeType(value, typeof(T));
+                }
+                catch (InvalidCastException invalidEx)
+                {
+                    Console.WriteLine(invalidEx);
+                    error = false;
+                }
+                catch (FormatException formatEx)
+                {
+                    Console.WriteLine(formatEx);
+                    error = false;
+                }
+                catch (OverflowException overEx)
+                {
+                    Console.WriteLine(overEx);
+                    error= false;
+                }
+                catch (ArgumentNullException argumentEx)
+                {
+                    Console.WriteLine(argumentEx);
+                    error= false;
+                }
+            }
+            return default(T);
         }
 
-        public static T QuestionForPerson<T>(string txt)
+        public static T QuestionForPerson<T>(string questionText)
         {
-            Console.Write($"{txt}: ");
-            T x = ConvertTypeForQuestion<T>(Console.ReadLine());
-            return x;
+            try
+            {
+                Console.Write($"{questionText}: ");
+                T x = ConvertTypeForQuestion<T>(Console.ReadLine());
+                return x;
+            }
+            catch (IOException ioEx)
+            {
+                Console.WriteLine(ioEx);
+            }
+            catch (OutOfMemoryException memoryEx)
+            {
+                Console.WriteLine(memoryEx);
+            }
+            catch (ArgumentOutOfRangeException rangeEx)
+            {
+                Console.WriteLine(rangeEx);
+            }
+            return default(T);
         }
     }
 }
