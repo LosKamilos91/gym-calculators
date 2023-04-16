524735f83874a39155426c92481c7aacadd4d60f
Kamil Orzechowski <k91.orzechowski@gmail.com>
2023-04-16 18:49:55 +0200

    Modified Program.cs
    Add questions for person about age, weight and growth

--- gym-calculators/gym-calculators/Program.cs
+++ gym-calculators/gym-calculators/Program.cs
@@ -1,5 +1,25 @@
 ﻿using gym_calculators.MainMenu;
 using gym_calculators.Other;
 
+int minimumValueForAge = 10;
+int minimumValueForWeight = 30;
+int minimumValueForGrowth = 100;
 Menu menu = new Menu("Hi! Let's first we need few information about you.");
 var age = QuestionsForPerson.QuestionForPerson<byte>("Age");
+while (age < minimumValueForAge || byte.MaxValue - 155 < age)
+{
+    Console.WriteLine($"Wrong value ({minimumValueForAge} - {byte.MaxValue - 155}). Try Again! ");
+    age = QuestionsForPerson.QuestionForPerson<byte>("Age");
+}
+var weight = QuestionsForPerson.QuestionForPerson<byte>("Weight");
+while (weight < minimumValueForWeight || 200 < weight)
+{
+    Console.WriteLine($"Wrong value ({minimumValueForWeight} - 200). Try Again! ");
+    weight = QuestionsForPerson.QuestionForPerson<byte>("Weight");
+}
+var growth = QuestionsForPerson.QuestionForPerson<byte>("Growth");
+while (growth < minimumValueForGrowth || byte.MaxValue < growth)
+{
+    Console.WriteLine($"Wrong value ({minimumValueForGrowth} - {byte.MaxValue}). Try Again! ");
+    growth = QuestionsForPerson.QuestionForPerson<byte>("Growth");
+}
