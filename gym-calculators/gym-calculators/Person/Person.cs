decfd66e838464ba26cdc95e9b1f1c5827796c7a
Kamil Orzechowski <k91.orzechowski@gmail.com>
2023-04-16 18:52:52 +0200

    Added new class
    Person.cs

---
+++ gym-calculators/gym-calculators/Person/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace gym_calculators.Person
+{
+    internal class Person
+    {
+        public Person(byte age, float weight, byte growth)
+        {
+            Age = age;
+            Weight = weight;
+            Growth = growth;
+        }
+        public byte Age { get; set; }
+        public float Weight { get; private set; }
+        public byte Growth { get; private set; }
+    }
+}
