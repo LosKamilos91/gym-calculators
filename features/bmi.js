47ca86d944d1cd0196bf2566a994452f76a8e5b1
Kamil Orzechowski <k91.orzechowski@gmail.com>
2024-09-21 11:22:05 +0200

    Add tests for BMI and implementation functions

--- features/bmi.js
+++ features/bmi.js
@@ -1,15 +1,72 @@
 'use strict';
 
-function calculateBMI() {
-    // TODO
+let bmi;
+
+function calculateBMI(bodyWeight, growth) {
+    bmi = (bodyWeight / (growth / 100) ** 2).toFixed(2);
+
+    return `Your BMI: ${bmi} - ${normsBMI()}`;
 }
 
 function commandCLI() {
-    // TODO
+    const args = process.argv.slice(2);
+    const bodyWeight = args[0];
+    const growth = args[1];
+
+    if (bodyWeight === undefined || growth === undefined) {
+        return 'Usage: npm run bmi <bodyWeight> <growth>';
+    }
+
+    if (bodyWeight === '') {
+        return 'Error: <bodyWeight> cannot be empty';
+    }
+
+    if (growth === '') {
+        return 'Error: <growth> cannot be empty';
+    }
+
+    if (typeof bodyWeight === 'string' || typeof growth === 'string') {
+        const weight = bodyWeight;
+        const grw = growth;
+
+        if (isNaN(weight)) {
+            return 'Error: <bodyWeight> cannot be text';
+        } else if (isNaN(grw)) {
+            return 'Error: <growth> cannot be text';
+        } else {
+            return calculateBMI(bodyWeight, growth);
+        }
+    }
+
+    if (bodyWeight < 10) {
+        return 'Error: Incorrect value for <bodyWeight>. Minimum value is 10';
+    } else if (growth < 100) {
+        return 'Error: Incorrect value for <growth>. Minimum value is 100';
+    } else if (bodyWeight > 250) {
+        return 'Error: Incorrect value for <bodyWeight>. Maximum value is 250';
+    } else if (growth > 230) {
+        return 'Error: Incorrect value for <growth>. Maximum value is 230';
+    } else {
+        return calculateBMI(bodyWeight, growth);
+    }
 }
 
 function normsBMI() {
-    //
+    if (bmi < 18.5) {
+        return 'Underweight';
+    } else if (bmi >= 18.5 && bmi < 25) {
+        return 'Correct';
+    } else if (bmi >= 25 && bmi < 30) {
+        return 'Overweight';
+    } else if (bmi >= 30 && bmi < 35) {
+        return 'First degree obesity';
+    } else if (bmi >= 35 && bmi < 40) {
+        return 'Second degree obesity';
+    } else {
+        return 'Exceptional obesity';
+    }
 }
 
-module.exports = {};
+console.log(commandCLI());
+
+module.exports = { commandCLI };
--- tests/bmi.test.js
+++ tests/bmi.test.js
@@ -1,35 +1,133 @@
 'use strict';
 
-const {} = require('../features/bmi');
+const { commandCLI } = require('../features/bmi');
+
+const url = '../features/bmi.js';
 
 describe('Display result', () => {
-    it('BMI should return "Underweight" result', () => {});
+    const originalArgv = process.argv;
 
-    it('BMI should return "Correct" result', () => {});
+    afterEach(() => {
+        process.argv = originalArgv;
+    });
 
-    it('BMI should return "Overweight" result', () => {});
+    it('BMI should return "Underweight" result', () => {
+        process.argv = ['node', url, 50, 190];
 
-    it('BMI should return "I degree obesity" result', () => {});
+        expect(commandCLI()).toMatch('Underweight');
+    });
 
-    it('BMI should return "II degree obesity" result', () => {});
+    it('BMI should return "Correct" result', () => {
+        process.argv = ['node', url, 70, 190];
 
-    it('BMI should return "Exceptional obesity" result', () => {});
+        expect(commandCLI()).toMatch('Correct');
+    });
+
+    it('BMI should return "Overweight" result', () => {
+        process.argv = ['node', url, 100, 190];
+
+        expect(commandCLI()).toMatch('Overweight');
+    });
+
+    it('BMI should return "First degree obesity" result', () => {
+        process.argv = ['node', url, 110, 190];
+
+        expect(commandCLI()).toMatch('First degree obesity');
+    });
+
+    it('BMI should return "Second degree obesity" result', () => {
+        process.argv = ['node', url, 130, 190];
+
+        expect(commandCLI()).toMatch('Second degree obesity');
+    });
+
+    it('BMI should return "Exceptional obesity" result', () => {
+        process.argv = ['node', url, 150, 190];
+
+        expect(commandCLI()).toMatch('Exceptional obesity');
+    });
 });
 
 describe('BMI commands', () => {
-    it('Should return use if not all arguments are specified', () => {});
+    const originalArgv = process.argv;
 
-    it('Should return message if <bodyWeight> is string', () => {});
+    afterEach(() => {
+        process.argv = originalArgv;
+    });
 
-    it('Should return message if <growth> is string', () => {});
+    it('Should return use if no argument has been entered', () => {
+        process.argv = ['node', url];
+
+        expect(commandCLI()).toBe('Usage: npm run bmi <bodyWeight> <growth>');
+    });
+
+    it('Should return use when only one value has been entered', () => {
+        process.argv = ['node', url, 50];
+
+        expect(commandCLI()).toBe('Usage: npm run bmi <bodyWeight> <growth>');
+    });
+
+    it('Should return message if <bodyWeight> is a text', () => {
+        process.argv = ['node', url, 'test', 190];
+
+        expect(commandCLI()).toBe('Error: <bodyWeight> cannot be text');
+    });
+
+    it('Should return message if <growth> is text', () => {
+        process.argv = ['node', url, 90, 'test'];
+
+        expect(commandCLI()).toBe('Error: <growth> cannot be text');
+    });
+
+    it('Should return message if <bodyWeight> is empty', () => {
+        process.argv = ['node', url, '', 190];
+
+        expect(commandCLI()).toBe('Error: <bodyWeight> cannot be empty');
+    });
+
+    it('Should return message if <growth> is empty', () => {
+        process.argv = ['node', url, 90, ''];
+
+        expect(commandCLI()).toBe('Error: <growth> cannot be empty');
+    });
 });
 
 describe('Function arguments', () => {
-    it('Should return message if <bodyWeight> less than 10', () => {});
+    const originalArgv = process.argv;
 
-    it('Should return message if <bodyWeight> greater than 250', () => {});
+    afterEach(() => {
+        process.argv = originalArgv;
+    });
 
-    it('Should return message if <growth> less than 100', () => {});
+    it('Should return message if <bodyWeight> less than 10', () => {
+        process.argv = ['node', url, 9, 190];
 
-    it('Should return message if <growth> greater than 230', () => {});
+        expect(commandCLI()).toBe(
+            'Error: Incorrect value for <bodyWeight>. Minimum value is 10'
+        );
+    });
+
+    it('Should return message if <bodyWeight> greater than 250', () => {
+        process.argv = ['node', url, 250.1, 190];
+
+        expect(commandCLI()).toBe(
+            'Error: Incorrect value for <bodyWeight>. Maximum value is 250'
+        );
+    });
+
+    it('Should return message if <growth> less than 100', () => {
+        process.argv = ['node', url, 90, 99.99];
+
+        expect(commandCLI()).toBe(
+            'Error: Incorrect value for <growth>. Minimum value is 100'
+        );
+    });
+
+    it('Should return message if <growth> greater than 230', () => {
+        process.argv = ['node', url, 90, 230.1];
+
+        expect(commandCLI()).toBe(
+            'Error: Incorrect value for <growth>. Maximum value is 230'
+        );
+    });
 });
